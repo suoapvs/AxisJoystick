920dbbd10be08c6d3ce51f7759f7ace17eff8237
Yurii Salimov <yuriy.alex.salimov@gmail.com>
2018-02-06 13:29:14 +0200

    Added a new examples.

---
+++ examples/MultipleReading/MultipleReading.ino
@@ -0,0 +1,57 @@
+/*
+  Axis Joystick, Multiple Reading
+
+  Reads a press of the joystick and displays information
+  in the default Serial.
+
+  Multiple reading of the joystick controller.
+  If the joystick is clamped,
+  returns a pressed button value.
+  Return value of pressing the remote control:
+    Move::PRESS - button is pressed;
+    Move::UP - X axis is pressed up;
+    Move::DOWN - X axis is pressed down;
+    Move::RIGTH - Y axis is pressed right;
+    Move::LEFT - Y axis is pressed left;
+    Move::NOT - not pressed.
+
+  https://github.com/YuriiSalimov/AxisJoystick
+
+  Created by Yurii Salimov, February, 2018.
+  Released into the public domain.
+*/
+#include <AxisJoystick.h>
+
+#define SW_PIN  5
+#define VRX_PIN A1
+#define VRY_PIN A2
+
+AxisJoystick* joystick;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(9600);
+  joystick = new AxisJoystick(SW_PIN, VRX_PIN, VRY_PIN);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  const AxisJoystick::Move move = joystick->multipleRead();
+  String moveTitle;
+  if (move == AxisJoystick::Move::PRESS) {
+    moveTitle = "PRESS";
+  } else if (move == AxisJoystick::Move::UP) {
+    moveTitle = "UP";
+  } else if (move == AxisJoystick::Move::DOWN) {
+    moveTitle = "DOWN";
+  } else if (move == AxisJoystick::Move::RIGHT) {
+    moveTitle = "RIGHT";
+  } else if (move == AxisJoystick::Move::LEFT) {
+    moveTitle = "LEFT";
+  } else {
+    moveTitle = "NOT";
+  }
+  Serial.println("Joystick, Multiple Reading: " + moveTitle);
+  delay(500); // To delay the output of information.
+}
+
