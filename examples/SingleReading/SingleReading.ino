858c9121cfb585e227f8f41cf0e0b6d707911647
Thijs Triemstra <info@collab.nl>
2020-07-31 19:58:13 +0200

    fix method order

--- examples/SingleReading/SingleReading.ino
+++ examples/SingleReading/SingleReading.ino
@@ -29,21 +29,6 @@
 
 Joystick* joystick;
 
-// the setup function runs once when you press reset or power the board
-void setup() {
-  Serial.begin(9600);
-
-  joystick = new AxisJoystick(SW_PIN, VRX_PIN, VRY_PIN);
-}
-
-// the loop function runs over and over again forever
-void loop() {
-  Serial.print("Joystick, Single Reading: ");
-  Serial.println(moveTitle(joystick->singleRead()));
-
-  delay(500); // optionally, only to delay the output of information in the example
-}
-
 /**
   Return title of the input joystick move.
 */
@@ -64,4 +49,19 @@ String moveTitle(const Joystick::Move move) {
     default:
       return "???";
   }
+}
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  Serial.begin(9600);
+
+  joystick = new AxisJoystick(SW_PIN, VRX_PIN, VRY_PIN);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  Serial.print("Joystick, Single Reading: ");
+  Serial.println(moveTitle(joystick->singleRead()));
+
+  delay(500); // optionally, only to delay the output of information in the example
 }
